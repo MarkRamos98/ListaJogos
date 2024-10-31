e6377897e213e41d563e30591298595ef1acfb0b
MarkRamos98 <marcos.ramos123@live.com>
2024-10-31 20:17:42 -0300

    Atualização

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,14 +1,28 @@
 package br.com.etecia.myapp;
 
+import static java.security.AccessController.getContext;
+
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +34,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-
     }
+
+
+
+    private List<Adapter> lsgames;
+    RecyclerView recycleGames;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+
+        recycleGames = view.findViewById(R.id.recycleGames);
+
+        lsgames = new ArrayList<>();
+        lsgames.add(new Games("NomeMod",R.drawable.tesvskyrim);
+
+        //carregando adaptador
+        recycleGames adapter = new recycleGames(getContext(),lsgames);
+
+        recycleGames.setLayoutManager(new GridLayoutManager(getContext(),1));
+
+        recycleGames.setAdapter();
+    }
+
 }
